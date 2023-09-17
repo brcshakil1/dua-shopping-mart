ff1baf5e753a078b65fd4241aa73c37f9fc2d434
brcshakil1 <brcshakil72@gmail.com>
2023-09-17 11:58:59 +0600

    added component and data

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <h1>Haat <span>Ghaat</span></h1>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
